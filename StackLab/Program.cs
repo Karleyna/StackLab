01af2b356bd0c507e7f3084c7c1ad02b95aea75f
Кристина <@DESKTOP-14IAL7E>
2021-10-13 23:21:16 +0700

    Добавьте файлы проекта.

---
+++ StackLab/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using StackLib;
+
+namespace StackLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            MyStack<int> a = new MyStack<int>();
+            
+                for (int i = 0; i < 3; i++)
+                {
+
+                    a.Push(r.Next(10, 25));
+
+                }
+                a.Show();
+           
+            for (; ; )
+            {
+                Console.WriteLine("Enter count:\n1.Push elem\n2.Pop elem\n3.Push stack\n4.Pop stack\n5.Show stack\n6.-123 to exit");
+                int x = Convert.ToInt32(Console.ReadLine());
+                if ( x == -123) {
+                    break;
+                }
+                int y;
+                switch (x)
+                {
+                    case 1:
+                        Console.WriteLine("Enter count\n");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        a.Push(y);
+                        a.Show();
+                        break;
+                    case 2:
+                        a.Pop();
+                        a.Show();
+                        break;
+                    case 3:
+                        MyStack<int> b = new MyStack<int>();
+
+                        for (int i = 0; i < 3; i++)
+                        {
+
+                            b.Push(r.Next(1, 45));
+
+                        }
+                        b.Show();
+                        a.Push_stack(b);
+                        a.Show();
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Enter count deleted items: ");
+                            int countItems = Convert.ToInt32(Console.ReadLine());
+                            a.Pop_stack(countItems);
+                        }
+
+                        break; 
+                }
+            }
+
+            
+        }
+        
+    }
+}
---
+++ StackLib/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace StackLib
+{
+    public class MyStack<T>
+    {
+        public int count = 0;
+        private int index_top = 0;
+        private T[] array;
+
+        private void Delete() //функция удаления всего массива
+        {
+            array = null;
+        }
+        public MyStack()
+        {
+            count = 3;
+            array = new T[count];
+        }
+        public bool is_free()//проверка  хватает ли места в массиве
+        {
+            if (index_top < count) //индекс верхнего элемента сравниваем с размером
+                return true;
+            else return false;
+        }
+        public void Push(T new_elem)//вставка элемента в стек
+        {
+            if (!is_free())
+            {
+                Array.Resize(ref array, array.Length * 2);
+                count *= 2;
+            }
+            array[index_top] = new_elem;
+            index_top++;
+        }
+        public T Pop()//удаление элемента 
+
+        {
+            if (index_top < 1)
+                throw new Exception("Error!");
+            T top = array[index_top - 1];
+            index_top--;
+            return top;           
+        }
+       
+        public int size()//вывод текущего размера стека
+        {
+            return index_top;
+        }
+
+        public void Push_stack(MyStack<T> new_stack)//вставка стека в стек
+        {
+            int index = new_stack.index_top - 1;
+            Array.Resize(ref array, array.Length * 2);
+            count *= 2;
+            while (index >= 0)
+            {
+                if (!is_free())
+                {
+                    Array.Resize(ref array, array.Length * 2);
+                    count *= 2;
+                }
+
+                array[index_top] = new_stack.array[index];
+                index_top++;
+                index--;
+            }
+            
+        }
+        public MyStack<T> Pop_stack(int countDeleted)//удаление стека в стеке
+        {
+            T[] temp = new T[countDeleted];
+            MyStack<T> new_stack = new MyStack<T>();
+            for (int i = 0; i < countDeleted; i++)
+            {
+                temp[i] = this.Pop();
+                new_stack.Push(temp[i]);
+            }
+            return new_stack;
+        }
+
+        public void Show() //вывод стека
+        {
+            for (int i = 0; i < index_top; i++)
+            {
+                Console.Write($"{array[i]} "); 
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
